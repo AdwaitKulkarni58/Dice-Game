569c9d024345f27b7b25d309afc65dfb338f1c58
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2021-07-09 23:42:42 +0530

    Removed unnecessary comments

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;     //generating random n
 
 var randomImage1 = "dice" + randomNumber1 + ".png";          //finding image
 
-// var randomImageSource1 = "images/" +randomImage1;            //pinpointing image in the images folder
-
 document.querySelectorAll("img")[0].setAttribute("src", randomImage1);   //choosing image to be displayed
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;    //generating random number between 1 to 6 to simulate dice throw
 
 var randomImage2 = "dice" + randomNumber2 + ".png";          //finding image
-
-// var randomImageSource2 = "images/" +randomImage2;            //pinpointing image in the images folder
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImage2);    //choosing image to be displayed
 
